8068c2a7017f7310402026b56de42a8941956a09
jmarine <marine.jacolot@ynov.com>
2022-09-07 14:12:12 +0200

    klm

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for lettre := 97; lettre <= 122; lettre++ {
+	for lettre := 122; lettre >= 1297; lettre-- {
 		z01.PrintRune(rune(lettre))
 	}
 	z01.PrintRune('\n')
