70818a41e84f941f78326c27e3df94f3d614ca08
jmarine <marine.jacolot@ynov.com>
2022-09-07 17:08:58 +0200

    ejfzejfihbfuezvuezvduvekkk

--- printcomb2.go
+++ printcomb2.go
@@ -3,39 +3,40 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for a := 48; a <= 57; a++ {
-		for b := 48; b <= 57; b++ {
-			for c := 48; c <= 56; c++ {
-				for d := 48; d <= 57; d++ {
-					if a == c {
-						if b < d {
-							z01.PrintRune(a)
-							z01.PrintRune(b)
-							z01.PrintRune(32)
-							z01.PrintRune(c)
-							z01.PrintRune(d)
-							if a == 57 && b == 56 && c == 57 && d == 57 {
-								z01.PrintRune('\n')
-							} else {
-								z01.PrintRune(',')
-								z01.PrintRune(32)
-							}
-						}
-					} else if a < c {
-						z01.PrintRune(a)
-						z01.PrintRune(b)
-						z01.PrintRune(32)
-						z01.PrintRune(c)
-						z01.PrintRune(d)
-						if a == 57 && b == 56 && c == 57 && d == 57 {
-							z01.PrintRune('\n')
-						} else {
-							z01.PrintRune(',')
-							z01.PrintRune(32)
-						}
-					}
-				}
-			}
-		}
-	}
+    allDigits := [10]rune{48, 49, 50, 51, 52, 53, 54, 55, 56, 57}
+    for , a := range allDigits {
+        for , b := range allDigits {
+            for , c := range allDigits {
+                for , d := range allDigits {
+                    if a == c {
+                        if b < d {
+                            z01.PrintRune(a)
+                            z01.PrintRune(b)
+                            z01.PrintRune(32)
+                            z01.PrintRune(c)
+                            z01.PrintRune(d)
+                            if a == allDigits[9] && b == allDigits[8] && c == allDigits[9] && d == allDigits[9] {
+                                z01.PrintRune('\n')
+                            } else {
+                                z01.PrintRune(',')
+                                z01.PrintRune(32)
+                            }
+                        }
+                    } else if a < c {
+                        z01.PrintRune(a)
+                        z01.PrintRune(b)
+                        z01.PrintRune(32)
+                        z01.PrintRune(c)
+                        z01.PrintRune(d)
+                        if a == allDigits[9] && b == allDigits[8] && c == allDigits[9] && d == allDigits[9] {
+                            z01.PrintRune('\n')
+                        } else {
+                            z01.PrintRune(',')
+                            z01.PrintRune(32)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
