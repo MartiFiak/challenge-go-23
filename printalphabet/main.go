0168b632061cc012362e4461ac8f45c13d4cce83
jmarine <marine.jacolot@ynov.com>
2022-09-07 12:29:48 +0200

    go-piscine

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,9 +3,7 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-
-	for lettre := 97; lettre <= 122; lettre++ {
+	for lettre := 'a'; lettre <= 'z'; lettre++ {
 		z01.PrintRune(rune(lettre))
-		z01.PrintRune('\n')
 	}
 }
