b5bc7386f4c342c679b0bf90f29cec63d974606c
jmarine <marine.jacolot@ynov.com>
2022-09-07 14:06:38 +0200

    order

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for lettre := 'z'; lettre != 'a'; lettre++ {
+	for lettre := 'z'; lettre <= 'a'; lettre++ {
 		z01.PrintRune(rune(lettre))
 	}
 	z01.PrintRune('\n')
