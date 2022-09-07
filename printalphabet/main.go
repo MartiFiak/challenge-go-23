1498bd6170a5a2717f24ada4cedfc9e589f52fca
jmarine <marine.jacolot@ynov.com>
2022-09-07 14:09:42 +0200

    revalpha

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	for lettre := 'z'; lettre <= 'a'; lettre++ {
+	for lettre := 122; lettre >= 97; lettre-- {
 		z01.PrintRune(rune(lettre))
 	}
 	z01.PrintRune('\n')
