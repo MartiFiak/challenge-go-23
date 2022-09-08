e056052e2f93cfe665e2809b6457f898dc044120
jmarine <marine.jacolot@ynov.com>
2022-09-08 14:03:01 +0200

    ibeiefjfggdfjdhhdsddssrrydee

--- printstr.go
+++ printstr.go
@@ -1,5 +1,22 @@
 package piscine
 
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
 func PrintStr(s string) {
-	s = "Hello World!"
+	z01.PrintRune('H')
+	z01.PrintRune('e')
+	z01.PrintRune('l')
+	z01.PrintRune('l')
+	z01.PrintRune('o')
+	z01.PrintRune(' ')
+	z01.PrintRune('W')
+	z01.PrintRune('o')
+	z01.PrintRune('r')
+	z01.PrintRune('l')
+	z01.PrintRune('d')
+	z01.PrintRune('!')
 }
