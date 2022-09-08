8e464049ad490e1fb9de3dd9612e4e58295c36df
jmarine <marine.jacolot@ynov.com>
2022-09-08 14:09:04 +0200

    ibeiefjfggdfjdhhdsddssrrydeef

--- printstr.go
+++ printstr.go
@@ -7,16 +7,9 @@ import (
 )
 
 func PrintStr(s string) {
-	z01.PrintRune('H')
-	z01.PrintRune('e')
-	z01.PrintRune('l')
-	z01.PrintRune('l')
-	z01.PrintRune('o')
-	z01.PrintRune(' ')
-	z01.PrintRune('W')
-	z01.PrintRune('o')
-	z01.PrintRune('r')
-	z01.PrintRune('l')
-	z01.PrintRune('d')
-	z01.PrintRune('!')
+	hello := "Hello World!"
+	for _, s := range hello {
+		z01.PrintRune(rune(s))
 }
+	z01.PrintRune('\n')
+	
