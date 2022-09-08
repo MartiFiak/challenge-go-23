7cf49e360ecc8fb0d1ade17b35cc874cb3905a54
jmarine <marine.jacolot@ynov.com>
2022-09-08 10:25:38 +0200

    tyt

---
+++ test.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+
+		aString :="Hello"
+
+		aStringChangeable := []byte(aString)
+
+		aStringChangeable[0] = 'A'
+
+		aStringFinalized := string(aStringChangeable)
+
+		fmt.Println(aString)
+
+		fmt.Println(aStringChangeable)
+
+		fmt.Println(aStringFinalized)
+}
