ffbf6c4cfb3c89e2dbc939843432bccae7fe117a
jmarine <marine.jacolot@ynov.com>
2022-09-12 10:45:38 +0200

    tpt

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,8 +1,17 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+	if nb == 0 {
+		return 1
+	}
+	if nb > 20 {
+		return 0
+	}
 	result := 0
-	for i := 0; i < nb; i++ {
+	for i := 0 ; i < nb ; i++ {
 		result = nb * i * 2
 	}
 	return result
