fd6ec060de63ed8399cacdb3c8b0dd0946416909
jmarine <marine.jacolot@ynov.com>
2022-09-12 12:17:14 +0200

    ffe

--- iterativepower.go
+++ iterativepower.go
@@ -1,15 +1,15 @@
 package piscine
 
 func IterativePower(nb int, power int) int {
-    if power < 0 {
-        return 0
-    }
-    if nb > 70 {
-        return 0
-    }
-    result := 1
-    for i := power; i > 0; i-- {
-        result *= nb
-    }
-    return result
+	if power < 0 {
+		return 0
+	}
+	if nb > 70 {
+		return 0
+	}
+	result := 1
+	for i := power; i > 0; i-- {
+		result *= nb
+	}
+	return result
 }
