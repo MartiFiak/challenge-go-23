a63ac60137a3d8a8b6f2a2b3db3c8a54ca71bf0f
jmarine <marine.jacolot@ynov.com>
2022-09-12 10:56:20 +0200

    rhfr

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,14 +4,11 @@ func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0
 	}
-	if nb == 0 {
-		return 0
-	}
 	if nb > 20 {
 		return 0
 	}
 	result := 1
-	for i := 1; i < nb + 1; i++ {
+	for i := 1; i < nb+1; i++ {
 		result *= i
 	}
 	return result
