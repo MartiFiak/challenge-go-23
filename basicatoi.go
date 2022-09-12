87d281ef34a4b981303a38bc2d4a7dfb4c004c13
jmarine <marine.jacolot@ynov.com>
2022-09-12 12:23:00 +0200

    ffe

--- basicatoi.go
+++
@@ -1,9 +0,0 @@
-package piscine
-
-func BasicAtoi(s string) int {
-	nb := 0
-	for _, r := range s {
-		nb = nb*10 + int(r-48)
-	}
-	return nb
-}
--- ultimatepointone.go
+++
@@ -1,7 +0,0 @@
-package piscine
-
-func UltimatePointOne(n ***int) {
-	var a int = 1
-	var b *int = &a
-	***n = *b
-}
