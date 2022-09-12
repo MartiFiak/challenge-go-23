4ca8e91f4de4866f09bab807880bdb876e256929
jmarine <marine.jacolot@ynov.com>
2022-09-12 11:54:51 +0200

    erf

--- iterativepower.go
+++ iterativepower.go
@@ -4,9 +4,6 @@ func IterativePower(nb int, power int) int {
 	if power < 0 || power > 20{
 		return 0
 	}
-	if nb < 0 || nb > 20{
-		return 0
-	}
 	result := 1
 	for i := 1; i < nb; i++ {
 		result *= nb
