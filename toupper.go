f12707b9d16fdb01b5965fa048d8a9fb0b2e215c
jmarine <marine.jacolot@ynov.com>
2022-09-15 16:12:08 +0200

    toupper

---
+++ toupper.go
@@ -0,0 +1,14 @@
+package piscine
+
+func ToUpper(s string) string {
+	str := ""
+	srune := []rune(s)
+	for _, st := range srune {
+		if (st >= 97 && st <= 122) {
+			str += string(st - 32)
+		} else {
+			str += string(st)
+		}
+	}
+	return str
+}
