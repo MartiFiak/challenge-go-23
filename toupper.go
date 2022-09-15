ce793b1a12f63090bb4560af4b4ce8742c8191d6
jmarine <marine.jacolot@ynov.com>
2022-09-15 16:12:28 +0200

    toupper

--- toupper.go
+++ toupper.go
@@ -4,7 +4,7 @@ func ToUpper(s string) string {
 	str := ""
 	srune := []rune(s)
 	for _, st := range srune {
-		if (st >= 97 && st <= 122) {
+		if st >= 97 && st <= 122 {
 			str += string(st - 32)
 		} else {
 			str += string(st)
