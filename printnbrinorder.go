e7dc3451a5169875985c892ec7e5b3ef40dae80f
jmarine <marine.jacolot@ynov.com>
2022-09-15 21:41:27 +0200

    printnbrinorder

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -3,27 +3,22 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
-	if n == 0 {
-		z01.PrintRune(48)
-	}
-	var allunit []int
-	if n < 0 {
-		for n < 0 {
-			allunit = append(allunit, n%10*(-1))
-			n = n / 10
-		}
-	} else {
-		for n > 0 {
-			allunit = append(allunit, n%10)
-			n = n / 10
-		}
-	}
-	SortIntegerTable(allunit)
-	for _, c := range allunit {
-		z01.PrintRune(rune(c + 48))
-	}
-}
-
-func SortIntegerTable(allunit []int) {
-	panic("unimplemented")
+    if n == 0 {
+        z01.PrintRune(48)
+    }
+    var allunit []int
+    if n < 0 {
+        for n < 0 {
+            allunit = append(allunit, n%10*(-1))
+            n = n / 10
+        }
+    } else {
+        for n > 0 {
+            allunit = append(allunit, n%10)
+            n = n / 10
+        }
+    }
+    for _, c := range allunit {
+        z01.PrintRune(rune(c + 48))
+    }
 }
