037e26df205a8c4fc7976def75ff1b043ef6a261
jmarine <marine.jacolot@ynov.com>
2022-09-16 15:08:22 +0200

    printprogramname

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,17 +1,12 @@
-package printprogramname
+package PrintProgramName
 
 import (
-	"fmt"
-	"github.com/01-edu/z01.PrintRune"
 	"os"
+	"github.com/01-edu/z01.PrintRune"
 )
 
-func printprogramname() {
-	arguments := os.Args
+func PrintProgramName() {
+	programName := os.Args[0]
 
-	z01.PrintRune("Number of arguments: %v\n", len(arguments))
-
-	if len(arguments) >= 3 {
-		z01.PrintRune("argument# 3 is: %v", os.Args[2])
-	}
+	z01.PrintRune(programName)
 }
