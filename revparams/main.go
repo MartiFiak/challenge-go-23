37a630edd08b4288f205b5206ef54a1ad70cf7eb
jmarine <marine.jacolot@ynov.com>
2022-09-16 17:17:16 +0200

    revparams

---
+++ revparams/main.go
@@ -0,0 +1,16 @@
+package main
+import (
+    "os"
+    "github.com/01-edu/z01"
+)
+
+func main() {
+	argt := os.Argsfor i := len(argt) - 1; i > 0; i-- {
+		if i != 0 {
+			for _, k := range argt[i] {
+				z01.PrintRune(k)
+			}
+			z01.PrintRune('\n')
+		}
+	}
+}
