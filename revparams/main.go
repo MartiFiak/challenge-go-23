e8773a4e6d86bda19fa3ffa1d4f057a7ea1d1210
jmarine <marine.jacolot@ynov.com>
2022-09-16 17:18:14 +0200

    revparams

--- revparams/main.go
+++ revparams/main.go
@@ -5,7 +5,8 @@ import (
 )
 
 func main() {
-	argt := os.Argsfor i := len(argt) - 1; i > 0; i-- {
+	argt := os.Args
+	for i := len(argt) - 1; i > 0; i-- {
 		if i != 0 {
 			for _, k := range argt[i] {
 				z01.PrintRune(k)
