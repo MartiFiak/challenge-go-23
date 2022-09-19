6599e7d920ed828192b107299a393419d9a96846
jmarine <marine.jacolot@ynov.com>
2022-09-19 17:39:34 +0200

    makerange

---
+++ makerange.go
@@ -0,0 +1,12 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	if min >= max {
+		return nil
+	}
+	result := make([]int, max-min)
+	for i := 0; i < len(result); i++ {
+		result[i] = i + min
+	}
+	return result
+}
