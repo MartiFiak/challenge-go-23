3e178d2e24ba8556a34a5af33f8a4a0b871ca937
jmarine <marine.jacolot@ynov.com>
2022-09-20 12:31:24 +0200

    concatparams

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -1,16 +1,19 @@
 package piscine
 
 func SplitWhiteSpaces(s string) []string {
-	n := " "
-	wrd []string{}
-	str := ""
-	for a, i := range s {
-		if i == " " || i == "\n" || i == "\t" || str != ""{
-			wrd = append(wrd, str)
-			str = ""
-		} else {
-			str += i
-		}
-	}
-	return str
+    srune := []rune(s)
+    lst := []string{}
+    str := ""
+    for i := 0; i < len(srune); i++ {
+        if srune[i] == ' ' || srune[i] == '\t' || srune[i] == '\n' {
+            if str != "" {
+                lst = append(lst, str)
+            }
+            str = ""
+        } else {
+            str += string(srune[i])
+        }
+    }
+    lst = append(lst, str)
+    return lst
 }
