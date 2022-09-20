19d3bdffd613d29f15cd3075b743a1cc1463187b
jmarine <marine.jacolot@ynov.com>
2022-09-20 11:55:30 +0200

    concatparams

--- concatparams.go
+++ concatparams.go
@@ -2,9 +2,12 @@ package piscine
 
 func ConcatParams(args []string) string {
 	str := ""
-	for _, i := range args[0:] {
+	for a, i := range args[0:] {
 		str += i
-		str += string("\n")
+		if len(args)-1 != a {
+			str += string("\n")
+		}
+		
 	}
 	return str
 }
