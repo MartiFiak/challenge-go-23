4c70b0141399058e4fe412a3442366b531e7fddf
jmarine <marine.jacolot@ynov.com>
2022-09-23 08:59:16 +0200

    flags

--- flags/main.go
+++ flags/main.go
@@ -35,7 +35,7 @@ func Help() {
 	fmt.Println(" -i")
 	fmt.Println("	 This flag inserts the string into the string passed as argument.")
 	fmt.Println("--order")
-	fmt.Println("-o")
+	fmt.Println("  -o")
 	fmt.Println("	 This flag will behave like a boolean, if it is called it will order the argument.")
 }
 
