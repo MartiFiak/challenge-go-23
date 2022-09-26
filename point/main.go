571775cfa61fa2cbbc4dc2994873c3fa0f00cb15
jmarine <marine.jacolot@ynov.com>
2022-09-26 15:05:36 +0200

    point

--- point/main.go
+++ point/main.go
@@ -1,22 +1,27 @@
 package main
 
-import (
-	"fmt"
-)
-
-type point struct {
-	x, y int
-}
-
-func setPoint(ptr *point) {
-	ptr.x = 42
-	ptr.y = 21
-}
+import "github.com/01-edu/z01"
 
 func main() {
-	points := new(point)
+	p := "x = " + IntegerToString(42) + ", y = " + IntegerToString(21) + "\n"
+	for _, c := range p {
+		z01.PrintRune(c)
+	}
+}
 
-	setPoint(points)
-
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+func IntegerToString(nbr int) string {
+	snbr := ""
+	var allunit []int
+	if nbr == 0 {
+		return "0"
+	} else {
+		for nbr > 0 {
+			allunit = append(allunit, nbr%10)
+			nbr = nbr / 10
+		}
+	}
+	for c := len(allunit) - 1; c >= 0; c-- {
+		snbr += string(rune(allunit[c] + 48))
+	}
+	return snbr
 }
