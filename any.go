df96fba453f837ecf8ccb8d665fa94a379b7b348
jmarine <marine.jacolot@ynov.com>
2022-09-28 11:05:02 +0200

    any

--- any.go
+++ any.go
@@ -1,10 +1,10 @@
 package piscine
 
 func Any(f func(string) bool, a []string) bool {
-    for _, i := range a {
-        if f(i) == true {
-            return true
-        }
-    }
-    return false
+	for _, i := range a {
+		if f(i) == true {
+			return true
+		}
+	}
+	return false
 }
