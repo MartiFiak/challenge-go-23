dd8bc3018a3c3c5ccc84e4e37fd2c8d3ef7dc132
jmarine <marine.jacolot@ynov.com>
2022-09-28 12:08:38 +0200

    issorted

--- issorted.go
+++ issorted.go
@@ -11,7 +11,7 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 
 	for i := 1; i < length; i++ {
 		if !(f(a[i-1], a[i]) >= 0) {
-			descendant := false
+			descendant = false
 		}
 	}
 	for i := 1; i < length; i++ {
